aa5d630a2f73e68dfed882d0c0dc0dc08a9f2bc7
Volkan Güleç <162556926+volkangulecx@users.noreply.github.com>
2025-03-28 09:32:47 +0300

    Dice Application
    C# Dili ile geliştirilmiştir. Tavla benzeri oyunlar için kullanılabilir.

---
+++ ZarProjesi/Zar/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Zar zar = new Zar();
+            string devamMi;
+
+            do
+            {
+                Console.WriteLine("Zarı atmak için bir tuşa basınız.");
+                Console.ReadKey();
+                int sonuc = zar.ZarAt();
+                Console.WriteLine("Zarın değeri: " + sonuc);
+
+                Console.WriteLine("Devam etmek istiyor musunuz? (e/h)");
+                devamMi = Console.ReadLine();
+
+            } while (devamMi.ToLower() == "e");
+            Console.WriteLine("Oyun bitti.Çıkmak için bir tuşa basınız. ");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ZarProjesi/Zar/Zar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zar
+{
+    public class Zar
+    {
+
+        private Random random;
+        public Zar() 
+        {
+            random = new Random(); // Rastgele sayı üretimi için
+
+        }
+
+        public int ZarAt()
+        {
+            return random.Next(1, 7); // 1 ile 6 arasında rastgele sayı üretir
+        }
+    }
+}
